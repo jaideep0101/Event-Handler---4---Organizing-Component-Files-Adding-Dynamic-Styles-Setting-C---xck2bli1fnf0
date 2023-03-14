2340e2ab2e9063d12125799e04ac6193c99cf9f5
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2023-03-14 12:20:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 const App = () => {
 
   const handleDoubleClick = (event) =>{
-  
+  if(event.detail === 2){
+    console.log("I was double clicked");
+  }else{
+    console.log("I was not double clicked");
   }
   return (
     <div id="main">
